e40be93f524d5b5d39ff9db214eceac2f69d1080
Alexandro Hugo Lopes <karkekoisaaeh@outlook.com>
2023-09-27 23:49:26 -0300

    Add project files.

---
+++ AnotherMessage.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "I'll" << std::endl;
+	std::cout << "learn" << std::endl;
+	std::cout << "to" << std::endl;
+	std::cout << "code" << std::endl;
+	std::cout << "in" << std::endl;
+	std::cout << "C++" << std::endl;
+	std::cout << "language" << std::endl;
+	system("PAUSE");
+	return 0;
+}
